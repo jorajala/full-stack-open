9335fc5285af227819aacaef99b8cf19f1e19479
Joonas Rajala <joonas.rajala@protonmail.com>
2024-02-25 20:48:37 +0200

    eslint

--- part-3/phonebook-backend/.eslintrc.cjs
+++ part-3/phonebook-backend/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { node: true, es2020: true },
+  env: { commonjs: true, node: true, es2020: true },
   extends: ["eslint:recommended"],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
