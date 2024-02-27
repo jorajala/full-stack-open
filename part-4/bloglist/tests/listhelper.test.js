9e9386ccb7527236970924edf05cae70c785d0f6
Joonas Rajala <joonas.rajala@protonmail.com>
2024-02-27 16:51:40 +0200

    exercises 4.3-4.5

--- part-4/bloglist/tests/listhelper.test.js
+++ part-4/bloglist/tests/listhelper.test.js
@@ -1,10 +1,45 @@
-const { test /*describe*/ } = require("node:test");
+const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
+
+let testBlogs = [
+  {
+    title: "test1",
+    author: "test1",
+    url: "",
+    likes: 1,
+  },
+  {
+    title: "test2",
+    author: "test2",
+    url: "",
+    likes: 2,
+  },
+  {
+    title: "test3",
+    author: "test3",
+    url: "",
+    likes: 10,
+  },
+];
 
 test("dummy returns one", () => {
   const blogs = [];
 
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
+});
+
+describe("total likes", () => {
+  test("sum the likes of a list of blogs", () => {
+    let result = listHelper.totalLikes(testBlogs);
+    assert.strictEqual(result, 13);
+  });
+});
+
+describe("favorite blog", () => {
+  test("blog with highest favorites", () => {
+    let result = listHelper.favoriteBlog(testBlogs);
+    assert.deepStrictEqual(testBlogs[2], result);
+  });
 });
--- part-4/bloglist/utils/list_helper.js
+++ part-4/bloglist/utils/list_helper.js
@@ -3,6 +3,21 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  let total = blogs.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.likes,
+    0
+  );
+  return total;
+};
+
+const favoriteBlog = (blogs) => {
+  let max = Math.max(...blogs.map((blog) => blog.likes));
+  return blogs.find((blog) => blog.likes === max);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 };
