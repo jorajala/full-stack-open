76e4bc9f2122272f10b9a6a8111097b4cccbad30
Joonas Rajala <joonas.rajala@protonmail.com>
2024-06-18 11:41:51 +0300

    5.20

--- part-5/bloglist-e2e/cypress/e2e/bloglist_app.cy.js
+++ part-5/bloglist-e2e/cypress/e2e/bloglist_app.cy.js
@@ -3,13 +3,13 @@ describe("Bloglist ", function () {
     cy.visit("http://localhost:5173");
     cy.request("POST", "http://localhost:3003/api/testing/reset");
     const testUser = {
-      name: "Test user",
+      name: "Test User",
       username: "tuser",
       password: "asdf",
     };
     cy.request("POST", "http://localhost:3003/api/users/", testUser);
     const testUser2 = {
-      name: "Test user 2",
+      name: "Test User 2",
       username: "tuser2",
       password: "asdf",
     };
@@ -25,35 +25,32 @@ describe("Bloglist ", function () {
       cy.get("#username").type("tuser");
       cy.get("#password").type("asdf");
       cy.get("#login-button").click();
-
-      cy.contains("Test Tunk logged in");
+      cy.contains("Test User logged in");
     });
 
     it("Fails with wrong username", function () {
-      cy.get("#username").type("nope.avi");
+      cy.get("#username").type("invalid");
       cy.get("#password").type("asdf");
       cy.get("#login-button").click();
 
       cy.get(".error").contains("invalid username");
-      cy.get("html").should("not.contain", "Test Tunk logged in");
+      cy.get("html").should("not.contain", "Test User logged in");
     });
     it("Fails with wrong password", function () {
       cy.get("#username").type("tuser");
-      cy.get("#password").type("lmao");
+      cy.get("#password").type("invalid");
       cy.get("#login-button").click();
 
       cy.get(".error").contains("invalid password");
-      cy.get("html").should("not.contain", "Test Tunk logged in");
+      cy.get("html").should("not.contain", "Test User logged in");
     });
   });
   describe("When logged in", function () {
     beforeEach(function () {
-      cy.get("#username").type("tuser");
-      cy.get("#password").type("asdf");
-      cy.get("#login-button").click();
+      cy.login({ username: "tuser", password: "asdf" });
     });
 
-    it.only("Blogs can be created", function () {
+    it("Blogs can be created", function () {
       let title = "Test title";
       let author = "Tester";
       let url = "testurl";
@@ -62,8 +59,18 @@ describe("Bloglist ", function () {
       cy.get("#addblog-author").type(author);
       cy.get("#addblog-url").type(url);
       cy.get("#addblog-button").click();
-
       cy.get(".blog").eq(0).should("contain", "Test title Tester");
+    });
+
+    describe("When there is a blog", function () {
+      beforeEach(function () {
+        cy.createBlog({ title: "Test blog", author: "Tester", url: "testurl" });
+      });
+      it.only("It can be liked", function () {
+        cy.get(".blog").eq(0).contains("details").click();
+        cy.get(".blog-details").contains("like").click();
+        cy.get(".blog-details").contains("likes: 1");
+      });
     });
   });
 });
